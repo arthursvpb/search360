0aeeb8d75a8b20ad74fbe099e39347322160be5d
arthursvpb <arthurandroid15@gmail.com>
2025-02-17 15:03:50 -0300

    feat(api): initialize NestJS application

---
+++ api/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const bootstrap = async (): Promise<void> => {
+  try {
+    const app = await NestFactory.create(AppModule);
+
+    await app.listen(process.env.PORT ?? 3000);
+  } catch (error) {
+    console.error('❌ Error starting the application:', error);
+    process.exit(1);
+  }
+};
+
+void bootstrap();
