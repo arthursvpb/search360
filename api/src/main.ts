3303efb7c9983ee771b9b3bf0d4db1952af37956
arthursvpb <arthurandroid15@gmail.com>
2025-02-20 20:22:39 -0300

    chore(api): enable CORS for frontend integration

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,6 +5,12 @@ const bootstrap = async (): Promise<void> => {
   try {
     const app = await NestFactory.create(AppModule);
 
+    app.enableCors({
+      origin: '*',
+      methods: 'GET,POST',
+      allowedHeaders: 'Content-Type',
+    });
+
     await app.listen(process.env.PORT ?? 3000);
   } catch (error) {
     console.error('❌ Error starting the application:', error);
